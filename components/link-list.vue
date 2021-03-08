ab27f0ab12caa656e295727d4b66e4a184936ba6
Shengqian Zou <zsq1750819212@gmail.com>
2021-03-08 14:54:53 +0800

    8/3 noon update

--- components/link-list.vue
+++ components/link-list.vue
@@ -1,3 +1,33 @@
 <template>
-
+	<li class="mdui-item mdui-ripple">
+		<i v-if="img_src.length" class="mdui-list-item-avater mdui-icon material-icons">{{img_src}}</i>
+		<div v-else>error</div>
+		<div class="mdui-list-item-content">
+			<div class="mdui-list-item-title mdui-list-one-line">{{title}}</div>
+			<div class="mdui-list-item-content mdui-list-one-line" v-if="theContent.length">{{theContent}}</div>
+		</div>
+		<div :href="url" v-if="url.length"><i class="mdui-list-item-icon mdui-icon material-icons">arrow_forward</i></div>
+	</li>
 </template>
+<script>
+export default{
+	props:{
+		"img_src":{
+			type: String,
+			default: ""
+		},
+		"title":{
+			type: String,
+			default: ""
+		},
+		"theContent":{
+			type: String,
+			default: ""
+		},
+		"url":{
+			type: String,
+			default: ""
+		},
+	}
+}
+</script>
