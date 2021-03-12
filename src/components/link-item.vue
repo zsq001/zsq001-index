fd246965c95cc23877f2a3d8c3798c9750a56818
Shengqian Zou <zsq1750819212@gmail.com>
2021-03-12 21:52:24 +0800

    3/12 night update

--- src/components/link-item.vue
+++ src/components/link-item.vue
@@ -1,13 +1,13 @@
 <template>
-	<li class="mdui-item mdui-ripple">
-		<i v-if="img_src.length" class="mdui-list-item-avater mdui-icon material-icons">{{ img_src }}</i>
+	<li class="mdui-list-item mdui-ripple">
+		<i v-if="img_src.length" class="mdui-list-item-icon mdui-icon material-icons">{{ img_src }}</i>
 		<div v-else-if="imgr_src" class="mdui-list-item-avater">
 		<img :src="imgr_src"/>
 		</div>
 		<div v-else></div>
 		<div class="mdui-list-item-content">
 			<div class="mdui-list-item-title mdui-list-item-one-line">{{ title }}</div>
-			<div class="mdui-list-item-content mdui-list-item-one-line" v-if="theContent.length">{{ theContent }}</div>
+			<div class="mdui-list-item-text mdui-list-item-one-line" v-if="theContent.length">{{ theContent }}</div>
 		</div>
 		<a :href="url" v-if="url.length"><i class="mdui-list-item-icon mdui-icon material-icons">arrow_forward</i></a>
 	</li>
