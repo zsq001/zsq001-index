f253968e01434d30bb7c5c305068cef273cc4e6f
zsq001 <15276671309@163.com>
2021-07-19 19:22:49 +0800

    Update about-me.vue

--- src/components/about-me.vue
+++ src/components/about-me.vue
@@ -2,10 +2,11 @@
 	<div class="mdui-typo">
 		<div class="mdui-center mdui-typo-title about-card">
 			<p>大一鸽子</p>
-      <p>一只furry</p>
-      <p>各种意义上的萌新</p>
-      <p>喜欢mc等沙盒游戏</p>
-      <p>不太喜欢每句话带个emoji</p>
+			<p>一只furry</p>
+			<p>各种意义上的萌新</p>
+			<p>喜欢mc等沙盒游戏</p>
+			<p>不太喜欢每句话带个emoji</p>
+			<p>欢迎各位来扩列>w<</p>
 		</div>
 	</div>
 </template>
