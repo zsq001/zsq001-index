47c689df9cfda304c6d5fc0f173ff1b102fb7649
Shengqian Zou <zsq1750819212@gmail.com>
2021-10-06 20:22:33 +0800

    10.6Update

--- src/components/Beian.vue
+++ src/components/Beian.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="mdui-typo mdui-valign">
 		<div class="mdui-center">
-			<p>备案号：<a href="https://beian.miit.gov.cn">京ICP备2021027219-1号</a></p>
+			<p>ICP备案号：<a href="https://beian.miit.gov.cn" target="_blank">京ICP备2021027219-1号</a></p>
+			<p>萌备案号（非正式）：<a href="https://icp.gov.moe/?keyword=20210822" target="_blank">萌ICP备20210822号</a></p>
 		</div>
 	</div>
 </template>
